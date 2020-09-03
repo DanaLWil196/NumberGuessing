24ec879224d7d7e9c6bf09b4ea11fc57e5dc7043
Dana Wilson <danalwil@outlook.com>
2020-09-03 10:02:17 -0400

    Add project files.

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("To high! Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win =  true;
+                }
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.WriteLine("Press any key to finish.");
+            Console.ReadKey(true);
+        }
+    }
+}
